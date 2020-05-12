ace928601da4997edbf6c62819409feccebb3568
Madhuri Dodla <madhuri.d@indianic.com>
2020-05-12 19:53:58 +0530

    Added build folder

---
+++ build/precache-manifest.80b34f03b1abaf08f738563de88b0024.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d20133c65fab0c0fcc22c1290bffa61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b49569585530c3d62292",
+    "url": "/static/css/2.4db41e99.chunk.css"
+  },
+  {
+    "revision": "e8db4a5a6a6e27b56f99",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b49569585530c3d62292",
+    "url": "/static/js/2.59aa6803.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.59aa6803.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ced95f9759934ca92bb",
+    "url": "/static/js/3.a1d58b0d.chunk.js"
+  },
+  {
+    "revision": "275fe79abee3b697f1673c8bd9c58856",
+    "url": "/static/js/3.a1d58b0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ceeb7d568b00d5754af",
+    "url": "/static/js/4.aa345118.chunk.js"
+  },
+  {
+    "revision": "593a4dfffab976199fd2",
+    "url": "/static/js/5.7a1fffe4.chunk.js"
+  },
+  {
+    "revision": "e8db4a5a6a6e27b56f99",
+    "url": "/static/js/main.4b1cebdd.chunk.js"
+  },
+  {
+    "revision": "e120b27f21a41e4d5dae",
+    "url": "/static/js/runtime-main.094e8e2a.js"
+  }
+]);
